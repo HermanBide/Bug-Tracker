7f218045c1d453aac0247ca4a7cedebd05fafabe
hermanb <bideherman@yahoo.com>
2022-05-15 22:31:58 -0700

    imported redux, redux-thunk and created Form post and posts components

--- tracker/client/src/App.js
+++ tracker/client/src/App.js
@@ -1,31 +1,42 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Register from "./components/registration/Register";
-import Login from "./components/registration/Login";
-import Nav from "./components/Nav";
-import "./App.css";
+import React , {useEffect} from "react";
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import useDispatch from 'react-redux'
+
+import {getPosts } from './actions/posts'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+// import { Routes, Route } from "react-router-dom";
+import pixel from './images/pixel.jpg'
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  },[dispatch]);
+
   return (
-    <div>
-      {/* <Router> */}
-        <Nav />
-{/* 
-        <Switch>
+      <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography  className={classes.heading} variant="h2" align="center">Travels</Typography>
+        <img className={classes.image} src={pixel} alt="memories" height='60'/> 
 
-          <Route exact path="/"></Route>
-
-          <Route path="/Register">
-            <Register />
-          </Route>
-
-          <Route path="/Login">
-            <Login />
-          </Route>
-
-        </Switch> */}
-      {/* </Router> */}
-    </div>
+      </AppBar>
+      <Grow in>
+        <Container >
+          <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+      </Container>
   );
 };
 
--- tracker/client/src/index.js
+++ tracker/client/src/index.js
@@ -1,14 +1,23 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 import App from "./App";
 import "./index.css";
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <StrictMode>
+  <React.StrictMode>
+  <Provider store={store}>
+    
     <Router>
       <App />
     </Router>
-  </StrictMode>,
+  </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
